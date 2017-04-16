74c93a47762bf1fedc6a9f9db4408d951f11179d
huangjiang <1005542693@qq.com>
2017-04-16 22:25:25 +0800

    20170417home

--- algorithm/01背包问题.cpp
+++ algorithm/01背包问题.cpp
@@ -21,19 +21,12 @@
 		   时间复杂度达到O(n*2^n).
 		   子问题是f[i][j]表示将i个物品中选取若干件放入体积j的最大价值.
 		   则有 f[i][j] = max{f[i-1][j](没有选取i物品),f[i-1][j-si]+vi(选取i物品)}.
-		   其中f[0][j] = 0 j = 0..C ; f[i][0] = 0 i = 0..N ; 
-
-
+		   其中f[0][j] = 0 j = 0,s1,s2,s3,..sn,s1+s2,s1+s3,...C ; f[i][0] = 0 i = 0..N ; 
 		   
-	时间复杂度 :
-		空间复杂度 :
+		   
+	    时间复杂度 : O(n+1*n/2)=O(n^2)
+		空间复杂度 : 一般用二维数组表示矩阵,可是实际中运用到的是矩阵的一半空间.可以用节点方式表示,是否节省空间待议?
 		简化 : 
-
-	解决方案1 : 
-		优缺点 :
-		
-	解决方案2 : 
-		优缺点 : 
 
 	引申问题 : 1.01背包问题隐形要求是物品最多选择1次,要是可选多次呢?
 			   2.j物品有两个维度(sj,vj),sj累加<=C的情况下,求vj累加的最大值.如果有三个维度(x,y,z),x累加<=CX,Y累加<=CY,z累加的最大值呢?4个维度呢?
@@ -46,12 +39,37 @@
 
  */
 #include <iostream>
+#include <vector>
 using namespace std;
 //函数
+struct pack
+{
+	int N;
+
+
+};
+
+
+
+int  backpack()
+{
+
+}
+
+
+
+//平白开销多余空间,不妥
+//#define  MAXN  1000
+//int S[MAXN][MAXN]; 
+
+
 
 //测试
-/*int main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
+
+
+
 	system("pause");
 	return 0;
-}*/
+}
